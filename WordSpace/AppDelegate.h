98e35a3ff5eac3c12fecb834e3fd54dd430c6e4c
Trevor Foltz <trevorfoltz@gmail.com>
2013-10-13 15:19:19 -0700

    Initial Commit

---
+++ WordSpace/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WordSpace
+//
+//  Created by Trevlord on 10/13/13.
+//  Copyright (c) 2013 forevorware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WordSpace/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WordSpace
+//
+//  Created by Trevlord on 10/13/13.
+//  Copyright (c) 2013 forevorware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
